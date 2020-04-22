81e82fbddb54a292084c2b126db88acae2916521
Eshi44 <e.hameliervolino@gmail.com>
2020-04-22 19:14:06 -0400

    added schema, workout model, and export to workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,47 @@
+//require mongoose
 const mongoose = require("mongoose");
+//create schema
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+	day: {
+		type: Date,
+		default: Date.now,
+	},
+	exercises: [
+		{
+			type: {
+				type: String,
+				trim: true,
+				required: "Please enter an exercise type",
+			},
+			name: {
+				type: String,
+				trim: true,
+				required: "Please enter an exercise name",
+			},
+			duration: {
+				type: Number,
+				required: "Please enter an exercise duration in min.",
+			},
+			weight: {
+				type: Number,
+			},
+			reps: {
+				type: Number,
+			},
+			sets: {
+				type: Number,
+			},
+			distance: {
+				type: Number,
+			},
+		},
+	],
+});
+
+//creat Workout model
+const Workout = mongoose.model("Workout", workoutSchema);
+
+//export Workout
+module.exports = Workout;
