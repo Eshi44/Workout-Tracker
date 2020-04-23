285f561c97ee30d05534932bab79303fdf841269
Eshi44 <e.hameliervolino@gmail.com>
2020-04-23 15:00:09 -0400

    added put route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -37,7 +37,20 @@ router.post("/api/workouts", (req, res) => {
   });
 
 ///PUT api/workouts/:id
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
 
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 ///DELETE api/workouts
 
 
